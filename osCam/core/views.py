6abf9938b5c7ab0ff833a0914853cc4f558b157c
Avery <nbdavery@gmail.com>
2022-03-03 18:28:33 -0800

    added static directory, template directory, and home html file.

--- osCam/core/views.py
+++ osCam/core/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello, security camera dashboard under construction!</h1>')
+    return render(request, 'core/home.html')
