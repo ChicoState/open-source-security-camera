70c9f66c37734227009943c2a55b6409b9858b50
Avery <apound2@mail.csuchico.edu>
2022-04-05 23:25:23 -0700

    form for configuring camera is coming together. need to fix the .save() functionality of submitting the form. looking good so far.

--- osCam/userconfig/forms.py
+++ osCam/userconfig/forms.py
@@ -1,3 +1,6 @@
+from cProfile import label
+from ipaddress import ip_address
+from pydoc import describe
 from django.db import models
 from django.db.models import fields
 from django.forms import widgets
@@ -20,12 +23,44 @@ class NetworkEntryForm( ModelForm ):
 
 
 class CameraEntryForm( ModelForm ):
-    model_info = forms.CharField(label='description',
-        widget=forms.TextInput(attrs={'size':'80', 'class':"form-control"})
-        )
+    device_name = forms.CharField(max_length=255, required=True, 
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Backyard Cam', 
+            'class':"form-control"}))
+    ip_address = forms.CharField(max_length=255, 
+        widget=forms.TextInput(attrs={'placeholder': '10.0.0.94', 'class':"form-control"}))
+    port = forms.IntegerField(
+        widget=forms.TextInput(attrs={'placeholder': '80', 'class':"form-control"}))
+    recording = forms.BooleanField(
+        label='enable recording',
+        widget=forms.CheckboxInput(),
+        required=False)
+    fps = forms.ChoiceField(
+        choices=((15, '15'), (30, '30'), (60, '60')),)
+    invert = forms.BooleanField(
+        required=False,
+        label='invert image',
+        widget=forms.CheckboxInput())
+    mirror = forms.BooleanField(
+        required=False,
+        label='mirror image',
+        widget=forms.CheckboxInput())
+    codec = forms.ChoiceField(
+        choices=(('h264', 'h264'), ('mjpeg', 'mjpeg'),),)
+    
+
     class Meta:
         model = Camera 
-        fields = ('model_info',)
+        fields = (
+            'device_name', 
+            'ip_address',
+            'port',
+            'recording',
+            'fps',
+            'invert',
+            'mirror',
+            'codec',)
+    
 
 # alternate carera form whoops
 # class CameraForm(forms.ModelForm):
