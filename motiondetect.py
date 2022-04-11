e350470f841e70f0d99948462b1e3896d58dbddc
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-04-11 12:28:33 -0700

    added construcor variables for easier control of recording and email notifications settings

--- motiondetect.py
+++ motiondetect.py
@@ -5,10 +5,28 @@ from cv2 import VideoWriter
 from datetime import datetime
 import time
 import os
+from cv2 import erode
+import numpy as np
+
 class MotionDetect():
+    def __init__(self):
+        #usb cam for testing, change for pi
+        # self.video = cv.VideoCapture(0)
+        self.codec = cv.VideoWriter_fourcc(*'XVID')
+        self.avg = None
+        self.out = None
+        self.numframes = 0
+        self.record = True
+        self.rotate = False
+        self.mirror = False
+        self.notify = True
+        self.showBox = True
+        self.showContours = False
+        self.fps = 30
+
     sendEmailNotification = True
     #recoding setup
-    def setRecording(fileName, frame):
+    def setRecording(self, fileName, frame):
         #type of codec (os dependent, currently working for ubunto 20.4)
         codec = cv.VideoWriter_fourcc(*'XVID')
         #where to save files
@@ -20,9 +38,34 @@ class MotionDetect():
         width, height, channels = frame.shape
         #return output object
         return fileName, filePath, cv.VideoWriter(filePath, codec, fps, (height, width))
+    def skeletonize(self, frame):
+        # Threshold the image
+        # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        ret,img = cv.threshold(frame, 200, 255, 0)
 
-    #works for images, recordings, live video
-    def rescaleFrame(frame, scale):
+        # Step 1: Create an empty skeleton
+        size = np.size(img)
+        skel = np.zeros(img.shape, np.uint8)
+
+        # Get a Cross Shaped Kernel
+        # element = cv.getStructuringElement(cv.MORPH_CROSS, (7,7))
+        element = cv.getStructuringElement(cv.MORPH_CROSS, (17,17))
+        # Repeat steps 2-4
+        tmp = 50
+        while tmp:
+            #Step 2: Open the image
+            open = cv.morphologyEx(img, cv.MORPH_OPEN, element)
+            #Step 3: Substract open from the original image
+            temp = cv.subtract(img, open)
+            #Step 4: Erode the original image and refine the skeleton
+            eroded = cv.erode(img, element)
+            skel = cv.bitwise_or(skel,temp)
+            img = eroded.copy()
+            # Step 5: If there are no white pixels left ie.. the image has been completely eroded, quit the loop
+            tmp-=1
+        cv.imshow("Skeleton",skel)
+        #works for images, recordings, live video
+    def rescaleFrame(self, frame, scale):
         #scale the width and height, (cast to int)
         width = int(frame.shape[1] * scale)
         height =int(frame.shape[0] * scale)
@@ -32,7 +75,7 @@ class MotionDetect():
 
     #convert frame to grey, compute Gaussian blur for noise reduction, update ave,
     #compute weighted averages, compute difference between wieghted ave and grayscale frame to get delta (background bodel - grayscale frame)
-    def imgProcess(frame, avg):
+    def imgProcess(self, frame, avg):
         #convert to grayscale image
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         #blur image to reduce noise (filter size 21X21)
@@ -44,11 +87,13 @@ class MotionDetect():
         cv.accumulateWeighted(gray, avg, 0.5)
         #compute difference between first frame and cur frame
         frameDelta = cv.absdiff(gray, cv.convertScaleAbs(avg))
-        thresh = cv.threshold(frameDelta, 2, 255, cv.THRESH_BINARY)[1]
+        thresh = cv.threshold(frameDelta, 5, 255, cv.THRESH_BINARY)[1]
         #dilate the threshold image to fill in holes
         dil = cv.dilate(thresh, None, iterations=2)
         #get contours
         cnts = cv.findContours(dil.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+        
         if len(cnts) == 2:
             cnts = cnts[0]
         elif len(cnts) == 3:
@@ -56,7 +101,15 @@ class MotionDetect():
         else:
             print("something went wrong, exiting program :(")
             exit()
+        # the below code is for potential future developments
+        # if self.showContours:
+        #     e = cv.erode(dil,(11,11), gray)
+        #     contours, hierarchy = cv.findContours(e, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        #     cv.drawContours(gray, contours, -1, (0,255,0), 3)
+        #     cv.imshow('conts', gray)
+        #     MotionDetect.skeletonize(self, gray)
         return avg, cnts
+
     def Detect(self):
         #innitial video capture: source ov vid or file path/name for usb cam
         capture = cv.VideoCapture(0)
@@ -66,12 +119,13 @@ class MotionDetect():
         #initialize video capture: source PiCam:
         avg = None
         #label videos based on date/time
-        date_time = datetime.now().strftime("%Y_%m_%d, %H:%M:%S")
+        date_time = datetime.now().strftime("%Y_%m_%d_%H:%M:%S")
         #get first frame
         isTrue, frame = capture.read()
-        fileName, filePath, out = MotionDetect.setRecording(date_time, frame)
-        #number of frames to record per video
-        numframes = 0
+        if self.record:
+            fileName, filePath, self.out = MotionDetect.setRecording(self, date_time, frame)
+            #number of frames to record per video
+            numframes = 0
         #infinate loop and capture video until 'd' is pressed
         while True:
             text = "searching..."
@@ -81,7 +135,7 @@ class MotionDetect():
                 print("could not receive image frame: shutting down")
                 break
             #update average frame and countour frame
-            avg, cnts = MotionDetect.imgProcess(frame, avg)
+            avg, cnts = MotionDetect.imgProcess(self, frame, avg)
             for c in cnts:
                 #if contours are less than desired area cont
                 if cv.contourArea(c) < 5000:
@@ -109,23 +163,38 @@ class MotionDetect():
             cv.imshow('Video', frame)
             #if motion detected record:
             if text == "Motion!":
-                numframes+=1
-                out.write(frame)
-                cv.imshow('recording', frame)
+                if self.record:
+                    numframes+=1
+                    self.out.write(frame)
+                    # cv.imshow('recording', frame)
             #if number of frames in recording is greater than 500 save recording and start new recording file
-            if numframes > 500:
-                numframes = 0
-                out.release()
-                camID = 1
-                #pass database info to subProcess
-                os.system('python send_email.py {} {} {} {}'.format(fileName, filePath, numframes, camID))
-                date_time = datetime.now().strftime("%Y_%m_%d, %H:%M:%S")
-                fileName, filePath, out= MotionDetect.setRecording(date_time, frame)
+            if self.record:
+                if numframes > 100:
+                    numframes = 0
+                    self.out.release()
+                    camID = 1
+                    #pass database info to subProcess
+                    fileName += '.avi'
+                    if self.notify:
+                        os.system('python send_email.py {} {} {} {}'.format(fileName, filePath, numframes, camID))
+                    date_time = datetime.now().strftime("%Y_%m_%d_%H:%M:%S")
+                    fileName, filePath, self.out= MotionDetect.setRecording(self, date_time, frame)
+            if self.showContours:
+                gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                gray = cv.GaussianBlur(gray, (7,7),0)
+                thresh = cv.threshold(gray, 100, 255, cv.THRESH_BINARY)[1]
+                dil = cv.dilate(thresh, None, iterations=2)
+                eroded = cv.erode(dil,(11,11), gray)
+                contours, hierarchy = cv.findContours(eroded, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+                cv.drawContours(gray, contours, -1, (0,255,0), 3)
+                # cv.drawContours(frame, contours, 3, (0,255,0), 3)
+                cv.imshow('solid', gray)
             #check for interupt
             if cv.waitKey(28) & 0xFF==ord('d'):
                 break
         #if escaped, releas video and destroy windows.
-        out.release()
+        if self.out != None:
+            self.out.release()
         capture.release()
         cv.destroyAllWindows()
         cv.waitKey(0)
