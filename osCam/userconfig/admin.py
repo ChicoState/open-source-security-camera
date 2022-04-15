f5947297e1c032c41d612098d8c4787cb4969eed
Avery <apound2@mail.csuchico.edu>
2022-04-15 16:43:25 -0700

    Modified userconfig/admin.py so that we finally see all models in the django admin page.
    I have to import models right above where they are used, I'm not sure why this is but it is.

--- osCam/userconfig/admin.py
+++ osCam/userconfig/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Network, RaspberryPi, Camera
+from .models import *
 # Register your models here.
 admin.site.register(Network)
 admin.site.register(RaspberryPi)
 admin.site.register(Camera)
+admin.site.register(CameraView)
+admin.site.register(Storage)
