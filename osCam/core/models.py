c9db6b79a0d030441e5e5a17181150167f60f621
kevdouglass <kevdouglass@gmail.com>
2022-04-22 22:37:55 -0700

    (1) Modularized our HTML template structure since all pages were using nav-bar and needed pre-fetched CSS/JS. (2) Created a new Directory called 'SHARED' template that all other templates 'EXTEND' using class based inheritance. (3) updated URL endpoints in navbar to be DYNAMIC URL such that if component is later moved, the developer has no need to reconfigure relative path for each link(endpoint). (4) Started implementation of File Path Builder. this PR includes a basic button-grouping displayed on home.html (needs linking to database Path model, coming soon).(5) Modified the view that represents our template for home.html such that it passes Path Data as context to the template.

--- osCam/core/models.py
+++ osCam/core/models.py
@@ -1,24 +1,9 @@
 #core/models.py
-
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
 
 class Recording(models.Model):
-    #recordedOn = models.DateTimeField(auto_now_add=True)
     recordingLength = models.IntegerField(blank=True, null=True)
     fileName = models.CharField(max_length=40, blank=True, null=True)
     filePath = models.CharField(max_length=40, blank=True, null=True)
     cameraId = models.ForeignKey("userconfig.Camera", on_delete=models.CASCADE, blank=True, null=True)
-
-# class NextPath(models.Model):
-#     data = models.CharField(default="", max_length=500, blank=True, null=True)
-#     # path = models.ForeignKey(Path, on_delete=models.CASCADE, blank=True, null=True)
-
-# class Path(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-#     path = models.CharField(default='/', max_length=500,blank=True, null=True)
-#     next = models.ForeignKey(NextPath, on_delete=models.CASCADE, blank=True, null=True)
-
-
-
+    
--- osCam/streamin/urls.py
+++ osCam/streamin/urls.py
@@ -6,8 +6,8 @@ from . import views
 TYPE="stream"
 
 urlpatterns = [
-    path('', views.view_stream, name=f'{TYPE}-page'),
-    path('add/', views.add_stream, name=f"add-{TYPE}"),
+    path('', views.view_stream_gallery, name=f'{TYPE}-gallery'),
+    path('add/', views.add_stream, name=f"{TYPE}-add"),
     path('edit/<int:id>/', views.edit_stream, name=f"edit-{TYPE}"),
     path('remove/<int:id>/', views.remove_stream, name=f"remove-{TYPE}"),
 ]
