961f716930fa11eb3c87e3a090b171293681638a
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-04-26 11:22:46 -0700

    edited file to conform to linting standards

--- builddirpath.py
+++ builddirpath.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import os
-def GetPath(): 
+def GetPath():
+
     path = []
-    cur_path =''
+    cur_path = '/'
     while True:
-        cur_path =''
+        cur_path = '/'
         choice = input("choose directory:")
         if choice == 'back':
             if len(path) == 0:
@@ -14,15 +15,15 @@ def GetPath():
         elif choice != 'back':
             path.append(choice)
         for el in path:
-            cur_path+= '/' + el
+            cur_path += '/' + el
         if cur_path != '':
             print()
             print("cur path is: " + cur_path)
             print()
-            # print(os.listdir(cur_path))
+            nextpaths=[]
             for sub in os.listdir(cur_path):
                 subdir = os.path.join(cur_path, sub)
                 if os.path.isdir(subdir):
+                    nextpaths.append(sub)
                     print(sub)
-    return path
 print(GetPath())
