e22c652b5e005468112ce66b16135912fcba981b
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-04-26 08:48:59 -0700

    removed deleted models from admin registry

--- osCam/userconfig/admin.py
+++ osCam/userconfig/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-admin.site.register(Network)
-admin.site.register(RaspberryPi)
 admin.site.register(Camera)
 admin.site.register(CameraView)
 admin.site.register(Storage)
