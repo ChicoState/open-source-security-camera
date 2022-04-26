85b548d80627d1b5fc540cb24c529fff4d2da1c4
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-04-25 20:05:14 -0700

    removed a lot of unused code by reducing models, forms, view, and html from settings that either aren't being used or wont be used in the near future

--- osCam/userconfig/models.py
+++ osCam/userconfig/models.py
@@ -3,50 +3,19 @@ from urllib import request
 from django.db import models
 from django.contrib.auth.models import User
 
-class Network(models.Model):
-    homeIpAddress = models.CharField(max_length=32, null=True, blank=True)
-    homeNetmask = models.CharField(max_length=32, null=True, blank=True)
-    cameraIpAddress = models.CharField(max_length=32, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-
-    def __str__(self):
-        return self.homeIpAddress
-
-class RaspberryPi(models.Model):
-    # User can have Many RaspberryPie (1:Many)
-    user = models.ForeignKey( User, on_delete=models.CASCADE, null=True, blank=True )
-    #network can have many RaspberryPie (1:Many) (we could do manyToMany but assuming we're coding MVP model)
-    network = models.ForeignKey(Network, on_delete=models.DO_NOTHING, null=True, blank=True)
-    modelNum = models.IntegerField(blank=True, null=True)
-    modelName = models.CharField(max_length=40,blank=True, null=True)
-    username = models.CharField(max_length=40, default='admin')
-    password = models.CharField(max_length=40, default='admin')
-
-    def __str__(self):
-        return self.modelName
-
 class Camera(models.Model):
     user = models.ForeignKey( User, on_delete=models.CASCADE, null=True, blank=True )
-    raspberryPi = models.ForeignKey( RaspberryPi , on_delete=models.CASCADE, null=True, blank=True)
-    modelNum = models.IntegerField(blank=True, null=True)
-    modelName = models.CharField(max_length=40,blank=True, null=True)
-    cameraIndex = models.IntegerField(default=0, unique=False, editable=False)
     deviceName = models.CharField(max_length=40, default='Camera 01')
-    ipAddress = models.CharField(max_length=40, default='10.0.0.94')
-    port = models.IntegerField(default=80)
 
     def __str__(self):
         return self.deviceName
 
 class CameraView(models.Model):
     user = models.ForeignKey( User, on_delete=models.CASCADE, null=True, blank=True )
-    # camera = models.ForeignKey( Camera, on_delete=models.CASCADE, null=True, blank=True)
+    camera = models.ForeignKey( Camera, on_delete=models.CASCADE, null=True, blank=True)
     showMotionBoxes = models.BooleanField(default=False)
-    showContours = models.BooleanField(default=False)
     showText = models.BooleanField(default=False)
     text = models.CharField(max_length=255, default='Cam Name :: Date :: Time')
-    contrast = models.IntegerField(default=0, blank=True, null=True)
-    brightness = models.IntegerField(default=0, blank=True, null=True)
     recording = models.BooleanField(default=False, blank=True, null=True)
     fps = models.IntegerField(default=15)
     invert = models.BooleanField(default=False)
@@ -57,13 +26,10 @@ class CameraView(models.Model):
 
 class Storage(models.Model):
     recordToDevice = models.BooleanField(default=False)
-    recordToCloud = models.BooleanField(default=False)
     filePath = models.CharField(max_length=255, default='/home/pi/Videos')
     maxSpace = models.IntegerField(default=0, blank=True, null=True)
     timeToLive = models.IntegerField(default=0, blank=True, null=True)
-    archive = models.BooleanField(default=False, blank=True, null=True)
     lengthOfRecordings = models.IntegerField(default=0, blank=True, null=True)
-    codec = models.CharField(max_length=40, default='h264') 
 
     def __str__(self):
         return self.filePath
