ac9d41950168c3516c774ac0f8d4d590777cfa03
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-04-26 12:58:05 -0700

    edited core/ forms models to conform to linting standards and deleted unused form from directory

--- osCam/core/models.py
+++ osCam/core/models.py
@@ -1,9 +1,24 @@
-#core/models.py
 from django.db import models
 
+
 class Recording(models.Model):
-    recordingLength = models.IntegerField(blank=True, null=True)
-    fileName = models.CharField(max_length=40, blank=True, null=True)
-    filePath = models.CharField(max_length=40, blank=True, null=True)
-    cameraId = models.ForeignKey("userconfig.Camera", on_delete=models.CASCADE, blank=True, null=True)
-    
+    recordingLength = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    fileName = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True
+    )
+    filePath = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True
+    )
+    cameraId = models.ForeignKey(
+        "userconfig.Camera",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
