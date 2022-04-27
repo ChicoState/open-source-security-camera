55598d9209ab197198e8cacd4bb4cb8920a0da18
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-04-26 18:43:15 -0700

    edited urls to adhere to linting standards

--- osCam/osCam/urls.py
+++ osCam/osCam/urls.py
@@ -13,13 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
-# from core import views as core_views
-# from user import views as user_views
-import streamin
-import userconfig as user_config
 
 urlpatterns = [
     path('admin/', admin.site.urls),
