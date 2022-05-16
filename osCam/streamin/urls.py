e28b8ed098e30203eea0822825a8b8b9ce0d135b
kevdouglass <kevdouglass@gmail.com>
2022-05-16 03:07:00 -0700

    updated streamin urls to reflect the necessary views.

--- osCam/streamin/urls.py
+++ osCam/streamin/urls.py
@@ -5,7 +5,6 @@ TYPE = "stream"
 
 urlpatterns = [
     path('', views.view_gallery_stream, name=f'gallery-{TYPE}'),
-    path('add/', views.CreateVideo.as_view(), name=f"create-{TYPE}"),
     path('edit/<int:pk>/', views.video_stream_edit, name=f"{TYPE}-update"),
-    path('remove/<int:pk>/', views.DeleteVideo.as_view(), name=f"{TYPE}-delete"),
+    path('remove/<int:pk>/', views.removeStream, name=f"{TYPE}-delete"),
 ] 
